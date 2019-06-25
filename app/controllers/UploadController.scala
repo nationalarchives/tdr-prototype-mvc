9e29fea666014ee2d0d8696f27d8ed7a330a3c08
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2019-06-25 13:31:20 +0100

    Get new auth code when the user visits the /upload page
    Repeat some of the authentication workflow to get a new code when
    the user visits /upload.
    
    This results in a redirect to a URL with ?code=abcd in the URL
    parameters. This parameter is accessible by the frontend, so the
    JavaScript can (in future) convert the code to a token just as it
    would in a purely client-side application.

--- app/controllers/UploadController.scala
+++ app/controllers/UploadController.scala
@@ -1,14 +1,34 @@
 package controllers
 
 import javax.inject._
+import net.ceedubs.ficus.Ficus._
+import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+import play.api.Configuration
 import play.api.mvc._
 
 @Singleton
 class UploadController @Inject()(
-  controllerComponents: ControllerComponents
+  controllerComponents: ControllerComponents,
+  configuration: Configuration
 ) extends AbstractController(controllerComponents) {
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.upload())
+    val codeOption = request.queryString.get("code").flatMap(_.headOption)
+
+    val cognitoConfig = configuration.underlying.as[CognitoUploadConfig]("cognito.upload")
+
+    // TODO: Add state parameter? How do we validate it?
+    val url = s"${cognitoConfig.authenticationUrl}?response_type=code&scope=${cognitoConfig.scopes}&client_id=${cognitoConfig.clientId}&redirect_uri=${cognitoConfig.redirectUrl}"
+
+    codeOption match {
+      case Some(code) => Ok(views.html.upload())
+      case None => Redirect(url)
+    }
   }
 }
+
+case class CognitoUploadConfig(
+  authenticationUrl: String,
+  redirectUrl: String,
+  scopes: String,
+  clientId: String)
