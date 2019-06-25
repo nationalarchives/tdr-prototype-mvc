c4698bb2619fa31c099070cbad4f31753e766406
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2019-06-25 11:42:08 +0100

    Delete redundant authentication check
    Remove Silhouette authenticated request check from the dashboard.
    This is not necessary now that the SecurityFilter is applied to all
    requests.

--- app/controllers/DashboardController.scala
+++ app/controllers/DashboardController.scala
@@ -1,12 +1,7 @@
 package controllers
 
-import auth.DefaultEnv
-import com.mohiva.play.silhouette.api.Silhouette
-import com.mohiva.play.silhouette.api.actions.SecuredRequest
 import javax.inject._
 import play.api.mvc._
-
-import scala.concurrent.Future
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -14,12 +9,10 @@ import scala.concurrent.Future
   */
 @Singleton
 class DashboardController @Inject()(
-  cc: ControllerComponents,
-  silhouette: Silhouette[DefaultEnv]
+  cc: ControllerComponents
 )extends AbstractController(cc) {
 
-  def index() = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
-    Future.successful(Ok(views.html.dashboard()))
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.dashboard())
   }
-
 }
