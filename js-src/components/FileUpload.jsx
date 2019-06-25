29e256c2025fcd27179c6c1fca5ddb9db73fe044
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2019-06-25 17:29:38 +0100

    Use AWS SDK to get token from auth code
    We don't do anything with the token yet, but it might let us create
    the user details using the AWS SDK.

--- js-src/components/FileUpload.jsx
+++ js-src/components/FileUpload.jsx
@@ -1,4 +1,13 @@
 import React, { Component } from "react";
+import {
+    IAuthenticationDetailsData,
+    AuthenticationDetails,
+    CognitoUserPool,
+    ICognitoUserData,
+    CognitoUser
+} from "amazon-cognito-identity-js";
+import AWS from "aws-sdk";
+import CognitoIdentityServiceProvider from "aws-sdk/clients/cognitoidentityserviceprovider";
 
 class FileUpload extends Component {
     constructor(props) {
@@ -10,6 +19,35 @@ class FileUpload extends Component {
         const url = window.location.href;
         const codeRegex = /.*?code\=([\w-]+)/;
         const awsCode = codeRegex.exec(url)[1];
+
+        const tokenEndpoint = "https://tdr.auth.eu-west-2.amazoncognito.com/oauth2/token";
+
+        fetch(tokenEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `grant_type=authorization_code&client_id=2u2clbhcqnjaj3fn0jaid078ao&redirect_uri=http://localhost:9000/upload&code=${awsCode}`
+        })
+            .then(res => {
+                console.log("Got response");
+                console.log(res);
+            })
+            .catch(error => {
+                console.log("Error fetching token");
+                console.log(error);
+            });
+
+        const poolData = {
+            UserPoolId: "eu-west-2_6Mn0M2i9C",
+            ClientId: "2u2clbhcqnjaj3fn0jaid078ao"
+        };
+        const userPool = new CognitoUserPool(poolData);
+
+        const user = userPool.getCurrentUser();
+
+        console.log("Current user:");
+        console.log(user);
 
         this.setState({
            awsCode: awsCode
--- js-src/index.js
+++ js-src/index.js
@@ -3,13 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 window.onload = function () {
-    console.log("Hello world!");
-
     const domContainer = document.querySelector('#file_upload_container');
+
     if (domContainer) {
-        console.log("Rendering FileUpload");
         ReactDOM.render(<FileUpload/>, domContainer);
-    } else {
-        console.log("No DOM container in which to render")
     }
 }
