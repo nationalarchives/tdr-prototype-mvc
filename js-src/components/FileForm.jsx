69d413ff7aacc19b9e3ca33a8b36a5438241fe50
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2019-06-26 16:19:06 +0100

    Detect when directory is selected in React component
    Just log the files for now.

--- js-src/components/FileForm.jsx
+++ js-src/components/FileForm.jsx
@@ -1,13 +1,33 @@
 import React, {Component} from "react";
 
 class FileForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleFileSelect = this.handleFileSelect.bind(this);
+    }
+
+    handleFileSelect(event) {
+        const files = event.target.files;
+        console.log("Files were selected");
+        console.log(files);
+    }
+
     render() {
         return (
             <div className="govuk-form-group">
                 <label className="govuk-label" htmlFor="upload-files">
                     Upload a file
                 </label>
-                <input className="govuk-file-upload" id="upload-files" name="upload-files" type="file" webkitdirectory="" />
+                <input
+                    className="govuk-file-upload"
+                    id="upload-files"
+                    name="upload-files"
+                    type="file"
+                    webkitdirectory=""
+                    onChange={this.handleFileSelect}
+                />
             </div>
         );
     }
