b5f855a339f3282bfb974ea08efafb72135f5e75
Sam Palmer <github@sampalmer.dev>
2019-07-23 15:56:15 +0100

    Add ecs update to deploy step

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@ docker build -t nationalarchives/prototype-play-app .
 docker push nationalarchives/prototype-play-app
 
 pip install awscli
+export PATH=$PATH:$HOME/.local/bin
 aws --version
+aws ecs update-service --cluster tdr-prototype-ecs-dev --service tdr-application-service-dev --force-new-deployment
