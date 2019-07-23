c34c020f4f29fd7ad38f8e61d5ba70fae4ac0869
Sam Palmer <github@sampalmer.dev>
2019-07-23 16:08:35 +0100

    put the docker service call earlier to solve wrong path problem

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t nationalarchives/prototype-play-app .
 docker push nationalarchives/prototype-play-app
 
-pip install awscli
+pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 aws --version
 aws ecs update-service --cluster tdr-prototype-ecs-dev --service tdr-application-service-dev --force-new-deployment
