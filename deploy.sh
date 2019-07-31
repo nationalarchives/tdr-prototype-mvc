26493a5c47ec69d9b049662493b88693cb5902d0
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-07-31 09:19:31 +0100

    Update travis file yml

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t nationalarchives/prototype-play-app:$1 .
-docker push nationalarchives/prototype-play-app
+docker push nationalarchives/prototype-play-app:$1
 
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
-aws --version
-aws ecs update-service --cluster tdr-prototype-ecs-dev --service tdr-application-service-dev --force-new-deployment
+aws ecs update-service --cluster tdr-prototype-ecs-$1 --service tdr-application-service-$1 --force-new-deployment
