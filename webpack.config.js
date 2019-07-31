f085dcf126de7c4e50132a45f9c868b8d7189cd8
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-07-31 11:32:41 +0100

    Test npm picking up env variables

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,9 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            TDR_BASE_URL: JSON.stringify(process.env.TDR_BASE_URL || "https://d1nd68699jelkh.cloudfront.net"),
+            TDR_BASE_URL: JSON.stringify(process.env.TDR_BASE_URL || "http://localhost:9000"),
             TDR_AUTH_URL: JSON.stringify(process.env.TDR_AUTH_URL || "https://tdr-dev.auth.eu-west-2.amazoncognito.com"),
-            TDR_USER_POOL_ID: JSON.stringify(process.env.TDR_USER_POOL_ID || "eu-west-2_lKXzGP3qg"),
+            TDR_USER_POOL_ID: JSON.stringify(process.env.TDR_USER_POOL_ID || "eu-west-2_6Mn0M2i9C"),
             TDR_IDENTITY_POOL_ID: JSON.stringify(process.env.TDR_IDENTITY_POOL_ID || "eu-west-2:4b26364a-3070-4f98-8e86-1e33a1b54d85"),
             UPLOAD_APP_CLIENT_ID: JSON.stringify(process.env.UPLOAD_APP_CLIENT_ID || "2m0vnd83hcbvnvvdmss1kkecec")
         })
