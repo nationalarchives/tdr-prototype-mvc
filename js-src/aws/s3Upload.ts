521e758caf0f1851ecae5eeccfaa73e297f4f360
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2019-09-03 15:16:52 +0100

    Use UUIDs as upload filenames rather than original filenames
    Get a UUID from the database for each file, and use it as the filename
    when uploading the file to S3.
    
    This will allow backend file checks and the file export task to link
    files in the API with files in S3.
    
    It also means that we don't have to encode file names that contain
    characters that aren't allowed in S3.

--- js-src/aws/s3Upload.ts
+++ js-src/aws/s3Upload.ts
@@ -7,10 +7,15 @@ declare var TDR_USER_POOL_ID: string;
 declare var TDR_IDENTITY_POOL_ID: string;
 declare var S3_UPLOAD_BUCKET: string;
 
-export const uploadFiles = (files: File[]) => {
+export interface UploadableFile {
+    id: string,
+    file: File
+}
+
+export const uploadFiles = (files: UploadableFile[]) => {
     const currentUser = getCurrentUser();
     
-    if(!currentUser) {        
+    if(!currentUser) {
         // Placeholder error handling. In Beta, we should reauthenticate the user.
         return Promise.reject("No current user");        
     }
@@ -28,25 +33,26 @@ export const uploadFiles = (files: File[]) => {
        
         const userName = currentUser.getUsername();
         const bucket = S3_UPLOAD_BUCKET;
-        const s3Uploadkey = `${userName}-` + uuidv4();
+        // TODO: Use the consignment ID once known
+        const parentFolder = `${userName}-${uuidv4()}`;
         const s3 = new S3({
             params: {
                 Bucket: bucket
             }
         });
 
-        const uploads = files.map(file => uploadFile(s3, bucket, file.name, s3Uploadkey, file));
+        const uploads = files.map(fileDetails => uploadFile(s3, bucket, fileDetails.id, parentFolder, fileDetails.file));
 
         // If any uploads fail, this will only return the first error. We should handle ALL errors, not just the first.
         return Promise.all(uploads);
     });
 };
 
-function uploadFile(s3: S3, bucket: string, name: string, s3UploadKey: string, content: File): Promise<void> {
+function uploadFile(s3: S3, bucket: string, name: string, parentFolder: string, content: File): Promise<void> {
     return new Promise((resolve, reject) => {
         s3.upload(
             {
-                Key: `${s3UploadKey}/${name}`,
+                Key: `${parentFolder}/${name}`,
                 Bucket: bucket,
                 Body: content
             },
