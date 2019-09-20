0765d7dbe958cf327e7d7caa6577ae097c10d756
Sam Palmer <github@sampalmer.dev>
2019-09-20 16:33:07 +0100

    Removed the old silhouette code

--- app/modules/TDRAttributes.scala
+++
@@ -1,8 +0,0 @@
-package modules
-
-import com.mohiva.play.silhouette.impl.providers.OAuth2Info
-import play.api.libs.typedmap.TypedKey
-
-object TDRAttributes {
-  val OAuthAccessTokenKey: TypedKey[OAuth2Info] = TypedKey[OAuth2Info]("accessTokens")
-}
