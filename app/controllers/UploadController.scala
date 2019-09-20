30e0d57a8e8da08abefbd62e1038a2747089aef0
Sam Palmer <github@sampalmer.dev>
2019-09-20 10:56:45 +0100

    Update controller to upload files from S3

--- app/controllers/UploadController.scala
+++ app/controllers/UploadController.scala
@@ -1,63 +1,70 @@
 package controllers
 
-import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import com.amazonaws.services.s3.model.PutObjectRequest
+import java.io.ByteArrayInputStream
+import java.nio.file.Files
+
+import actors.S3UploadActor
+import actors.S3UploadActor.S3Request
+import akka.actor.ActorSystem
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.model.{ObjectMetadata, PutObjectRequest}
 import graphql.GraphQLClientProvider
 import graphql.codegen.CreateMultipleFiles.createMultipleFiles.{Data, Variables, document}
 import graphql.codegen.types.CreateFileInput
+import io.circe.parser.decode
 import javax.inject._
 import play.api.Configuration
 import play.api.libs.json.{Json, _}
 import play.api.mvc._
 
-import scala.collection.immutable
-import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.concurrent.duration.Duration
+import scala.concurrent.{ExecutionContext, Future}
+
 
 @Singleton
 class UploadController @Inject()(
-  client: GraphQLClientProvider,
-  controllerComponents: ControllerComponents,
-  config: Configuration
-)(implicit val ex: ExecutionContext) extends AbstractController(controllerComponents) {
+                                  client: GraphQLClientProvider,
+                                  controllerComponents: ControllerComponents,
+                                  system: ActorSystem,
+                                  config: Configuration,
+                                )(implicit val ex: ExecutionContext) extends AbstractController(controllerComponents) {
 
-  private val accessKeyID : String =  config.get[String]("aws.access.key.id")
-  private val accessKeySecret : String =  config.get[String]("aws.secret.access.key")
+
+  private val accessKeyID: String = config.get[String]("aws.access.key.id")
+  private val accessKeySecret: String = config.get[String]("aws.secret.access.key")
+
+  private val s3Actor = system.actorOf(S3UploadActor.props, "s3-upload-actor")
 
   def index(consignmentId: Int) = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.upload())
-//    val codeOption = request.queryString.get("code").flatMap(_.headOption)
-//
-//    val cognitoConfig = configuration.underlying.as[CognitoUploadConfig]("cognito.upload")
-//
-//    // TODO: Add state parameter? How do we validate it?
-//    val url = s"${cognitoConfig.authenticationUrl}?response_type=code&scope=${cognitoConfig.scopes}&client_id=${cognitoConfig.clientId}&redirect_uri=${cognitoConfig.redirectUrl}"
-//
-//    codeOption match {
-//      case Some(code) => Ok(views.html.upload())
-//      case None => Redirect(url)
-//    }
+    Ok(views.html.upload(consignmentId))
   }
-  def upload = Action(parse.multipartFormData) { request =>
-    val a: String = request.body.dataParts("file-id-data").head
-    //Convert this to an object to do the file mapping and make the s3 upload async
+
+  def upload(consignmentId: Int) = Action(parse.multipartFormData) { request =>
+    val fileIdString: String = request.body.dataParts("file-id-data").head
+
+    val fileIdMap: Map[String, String] = decode[Map[String,String]](fileIdString) match {
+      case Right(r) => r
+      case Left(_) => Map()
+    }
+
     request.body
       .files
-      .map { file  =>
-        val s3Client = AmazonS3ClientBuilder.standard()
-          .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKeyID, accessKeySecret)))
-          .withRegion("eu-west-2")
-          .build()
-
-        val request = new PutObjectRequest("tdr-files-test", file.filename, file.ref)
-        s3Client.putObject(request)
+      .foreach { file =>
+        fileIdMap.get(file.filename) match {
+          case Some(id) =>
+            val fileArr: Array[Byte] = Files.readAllBytes(file.ref.path)
+            val metaData: ObjectMetadata = new ObjectMetadata()
+            metaData.setContentLength(fileArr.length)
+            val request = new PutObjectRequest("tdr-files-test", s"$consignmentId/$id", new ByteArrayInputStream(fileArr), metaData)
+            s3Actor ! S3Request(request, new BasicAWSCredentials(accessKeyID, accessKeySecret))
+          case None =>
+        }
       }
-      Ok("File uploaded")
+    Redirect(routes.FileStatusController.getFileStatus(consignmentId))
 
   }
 
   case class FileInputs(data: List[CreateFileInput])
+
   implicit val createFileInputReads: Reads[CreateFileInput] = Json.reads[CreateFileInput]
   implicit val fileInputReads: Reads[FileInputs] = Json.reads[FileInputs]
 
@@ -70,11 +77,9 @@ class UploadController @Inject()(
       },
       fileInputs => {
         val appSyncClient = client.graphqlClient(List())
-        appSyncClient.query[Data, Variables](document,Variables(fileInputs.data)).result.map {
+        appSyncClient.query[Data, Variables](document, Variables(fileInputs.data)).result.map {
           case Right(r) =>
-            Ok(Json.toJson(r.data.createMultipleFiles.map {
-              f => Map("id" -> f.id.toString, "path" -> f.path.toString)
-            }))
+            Ok(Json.toJson(r.data.createMultipleFiles map (f => f.path.toString -> f.id.toString) toMap))
           case Left(ex) => InternalServerError(ex.errors.toString())
         }
 
@@ -83,9 +88,3 @@ class UploadController @Inject()(
 
   }
 }
-
-case class CognitoUploadConfig(
-  authenticationUrl: String,
-  redirectUrl: String,
-  scopes: String,
-  clientId: String)
