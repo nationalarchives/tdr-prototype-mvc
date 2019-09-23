aacbf1a481f52f04ceffd753032e138dbd9d6aa5
Ian Hoyle <ian.hoyle@nationalarchives.gov.uk>
2019-09-23 15:02:57 +0100

    Moved ConsignmentData from form to model

--- app/controllers/CreateConsignmentController.scala
+++ app/controllers/CreateConsignmentController.scala
@@ -1,11 +1,9 @@
 package controllers
 
-import akka.http.scaladsl.model.headers.RawHeader
-import forms.ConsignmentForm
 import graphql.GraphQLClientProvider
 import graphql.codegen.CreateConsignment.createConsignment._
-
 import javax.inject.{Inject, _}
+import model.CreateConsignmentData
 import play.api.Configuration
 import play.api.mvc._
 
@@ -19,13 +17,13 @@ class CreateConsignmentController @Inject()(
                                              implicit val ex: ExecutionContext) extends AbstractController(controllerComponents) with play.api.i18n.I18nSupport {
 
   def index(seriesId:Int) = Action.async { implicit request: Request[AnyContent] =>
-    Future(Ok(views.html.createConsignments(ConsignmentForm.form, seriesId)))
+    Future(Ok(views.html.createConsignments(CreateConsignmentData.form, seriesId)))
   }
 
   def submit() = Action.async { implicit request =>
 
     val appSyncClient = client.graphqlClient(List())
-    val form = ConsignmentForm.form.bindFromRequest
+    val form = CreateConsignmentData.form.bindFromRequest
 
     val vars = Variables(form.get.consignmentName,form.get.seriesId,request.session.get("username").get,form.get.transferringBody)
     appSyncClient.query[Data,Variables](document, vars).result.map(result => result match {
--- app/model/CreateConsignmentData.scala
+++ app/model/CreateConsignmentData.scala
@@ -1,17 +1,17 @@
-package forms
+package model
 
 import play.api.data.Form
 import play.api.data.Forms._
 
-case class ConsignmentData(consignmentName: String, transferringBody: String, seriesId: Int)
+case class CreateConsignmentData(consignmentName: String, transferringBody: String, seriesId: Int)
 
-object ConsignmentData {
-  val form: Form[ConsignmentData] = Form(
+object CreateConsignmentData {
+  val form: Form[CreateConsignmentData] = Form(
     mapping(
       "consignmentName" -> text,
       "transferringBody" -> text,
       "seriesId" -> number
-    )(ConsignmentData.apply)(ConsignmentData.unapply)
+    )(CreateConsignmentData.apply)(CreateConsignmentData.unapply)
   )
 }
 
