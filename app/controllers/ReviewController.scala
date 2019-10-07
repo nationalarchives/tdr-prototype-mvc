1aebf2cc238fb2ae719c76006672552a72531a1a
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2019-10-07 11:17:28 +0100

    Show user the confirmation page when they confirm a transfer
    Link the confirmation form to the transfer confirmation page. This
    doesn't update any data or start any export tasks yet.

--- app/controllers/ReviewController.scala
+++ app/controllers/ReviewController.scala
@@ -1,35 +1,84 @@
 package controllers
 
+import com.mohiva.play.silhouette.api.Silhouette
 import graphql.GraphQLClientProvider
 import graphql.codegen.GetConsignment.getConsignment
+import graphql.codegen.GetConsignment.getConsignment.GetConsignment
 import graphql.codegen.GetFileStatus.getFileChecksStatus
+import graphql.codegen.GetFileStatus.getFileChecksStatus.GetFileChecksStatus
 import javax.inject.{Inject, _}
+import model.ReviewData
 import play.api.Configuration
+import play.api.data.Form
+import play.api.data.Forms.{mapping, text}
 import play.api.mvc._
+import utils.DefaultEnv
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 
 @Singleton
 class ReviewController @Inject()(
                                   client: GraphQLClientProvider,
                                   controllerComponents: ControllerComponents,
+                                  silhouette: Silhouette[DefaultEnv],
                                   configuration: Configuration)(
                                   implicit val ex: ExecutionContext) extends AbstractController(controllerComponents) with play.api.i18n.I18nSupport {
 
-  def index(consignmentId: Int) = Action.async {
+  val form = Form(
+    mapping(
+      "confirmRecordTransfer" -> text
+        .verifying("Must answer yes", s => hasAgreed(s)),
+      "confirmOpen" -> text
+        .verifying("Must answer yes", s => hasAgreed(s)),
+      "confirmTnaOwnership" -> text
+        .verifying("Must answer yes", s => hasAgreed(s)),
+    )(ReviewData.apply)(ReviewData.unapply)
+  )
+
+  def index(consignmentId: Int) = silhouette.SecuredAction.async {
     implicit request =>
-
-      val graphQlClient = client.graphqlClient
-
-      val consignmentDetails =  graphQlClient.query[getConsignment.Data, getConsignment.Variables](getConsignment.document, getConsignment.Variables(consignmentId)).result
-      val fileDetails = graphQlClient.query[getFileChecksStatus.Data, getFileChecksStatus.Variables](getFileChecksStatus.document, getFileChecksStatus.Variables(consignmentId)).result
-
-      for {
-        consignment <-consignmentDetails
-        files <- fileDetails
-      } yield {
-        Ok(views.html.review(consignment.right.get.data.getConsignment.get, files.right.get.data.getFileChecksStatus))
-      }
+      getDetailsToReview(consignmentId).map(reviewDetails =>
+        Ok(views.html.review(reviewDetails.consignment, reviewDetails.fileChecks, form))
+      )
   }
+
+  def submit(consignmentId: Int) = silhouette.SecuredAction.async { implicit request: Request[AnyContent] =>
+
+    val errorFunction: Form[ReviewData] => Future[Result] = { formWithErrors: Form[ReviewData] =>
+      getDetailsToReview(consignmentId).map(reviewDetails =>
+        BadRequest(views.html.review(reviewDetails.consignment, reviewDetails.fileChecks, formWithErrors))
+      )
+    }
+    val successFunction: ReviewData => Future[Result] = { formData: ReviewData =>
+      Future.apply(Redirect(routes.TransferConfirmationController.index()))
+    }
+
+    val formValidationResult: Form[ReviewData] = form.bindFromRequest
+
+    formValidationResult.fold(
+      errorFunction,
+      successFunction
+    )
+  }
+
+  private def hasAgreed(s: String): Boolean = {
+    s.equals("yes")
+  }
+
+  private def getDetailsToReview(consignmentId: Int): Future[ConsignmentReview] = {
+    val graphQlClient = client.graphqlClient
+
+    val consignmentDetails =  graphQlClient.query[getConsignment.Data, getConsignment.Variables](getConsignment.document, getConsignment.Variables(consignmentId)).result
+    val fileDetails = graphQlClient.query[getFileChecksStatus.Data, getFileChecksStatus.Variables](getFileChecksStatus.document, getFileChecksStatus.Variables(consignmentId)).result
+
+    for {
+      consignment <- consignmentDetails
+      files <- fileDetails
+    } yield {
+      ConsignmentReview(consignment.right.get.data.getConsignment.get, files.right.get.data.getFileChecksStatus)
+    }
+  }
+
+  case class ConsignmentReview(consignment: GetConsignment, fileChecks: GetFileChecksStatus)
 }
---
+++ app/model/ReviewData.scala
@@ -0,0 +1,3 @@
+package model
+
+case class ReviewData(confirmRecordTransfer: String, confirmOpen: String, confirmTnaOwnership: String)
