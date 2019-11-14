f2066a5dc714c97002d23f1dac8e79327db19d61
mkingsbury <mkingsbury@web.local>
2019-11-14 10:47:11 +0000

    scrappy works to create new user with pwd from sign up page

--- app/auth/UzerZervice.scala
+++ app/auth/UzerZervice.scala
@@ -3,23 +3,24 @@ package auth
 import forms.SignUpForm
 import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.regions.Regions
-import com.amazonaws.services.cognitoidp.model.{AdminCreateUserRequest, AdminCreateUserResult, AttributeType}
+import com.amazonaws.services.cognitoidp.model.{AdminCreateUserRequest, AdminCreateUserResult, AdminInitiateAuthRequest, AdminInitiateAuthResult, AdminRespondToAuthChallengeRequest, AttributeType, AuthFlowType}
 import com.amazonaws.services.cognitoidp.{AWSCognitoIdentityProvider, AWSCognitoIdentityProviderClientBuilder}
 
-
+import scala.collection.JavaConverters._
 
 class UzerZervice{
 
   //config from
   val cognitoID: String = "AKIATTD47MKYZZC6CJ6W"
   val cognitoKey: String = "Kw3440AMWqGED/aVWyXCwmH/0rBzvUhA3OI4Pipl"
+  val clientID: String = "5pekokle5lubuqokujaffk076f"  //"Your Cognito client ID goes here"
   val poolId: String = "eu-west-2_MV1Sze5fS"
   val region: Regions = Regions.EU_WEST_2
 
+  val tempPwd = "Pa$$w0rd"
 
   //names for things in congnito db...
-  val LOCATION = "custom:location"
-  val LASTNAME = "custom:lastname"
+  val LASTNAME = "custom:location" //TODO: fix naming in cognito
   val EMAIL = "email"
 
   val mIdentityProvider: AWSCognitoIdentityProvider = {
@@ -33,19 +34,41 @@ class UzerZervice{
     findUserByEmailAddr(userInfo.email) match {
       case Some(_) => throw new Exception("duplicate email: caller should check not already existing")
       case None =>
-        val cognitoReequest: AdminCreateUserRequest = new AdminCreateUserRequest()
+        val cognitoRequest: AdminCreateUserRequest = new AdminCreateUserRequest()
             .withUserPoolId(poolId)
+            .withTemporaryPassword(tempPwd)
             .withUsername(userInfo.firstName) //TODO = we have firstname
             .withUserAttributes(
               new AttributeType().withName(EMAIL).withValue(emailAddr),
-              new AttributeType().withName(LOCATION).withValue(userInfo.lastName),
+              new AttributeType().withName(LASTNAME).withValue(userInfo.lastName),
               new AttributeType().withName("email_verified").withValue("true")
             )
-        val xx: AdminCreateUserResult = mIdentityProvider.adminCreateUser(cognitoReequest)
+        val createdNewUserWithTempPasswordResult: AdminCreateUserResult = mIdentityProvider.adminCreateUser(cognitoRequest)
+        changePasswordForNewUser(userInfo.firstName, userInfo.password)
     }
   }
 
-  def findUserByEmailAddr(str: String): Option[SignUpForm.Data] = None
+  private def changePasswordForNewUser(userName: String, newPassword: String): Unit = {
+    //get a session id (with a NEW_PASSWORD_REQUEST challenge)
+    val authParams = Map("USERNAME" -> userName, "PASSWORD" -> tempPwd).asJava
+    val authRequest = new AdminInitiateAuthRequest()
+      .withAuthFlow(AuthFlowType.ADMIN_NO_SRP_AUTH)
+      .withUserPoolId(poolId)
+      .withClientId(clientID)
+      .withAuthParameters(authParams)
+    val authResult: AdminInitiateAuthResult = mIdentityProvider.adminInitiateAuth(authRequest)
+    val session = authResult.getSession
+    //with the session id we can change the password
+    val challengeResponseParams = Map("USERNAME" -> userName, "NEW_PASSWORD" -> newPassword).asJava
+    val request = new AdminRespondToAuthChallengeRequest()
+        .withUserPoolId(poolId)
+        .withClientId(clientID)
+        .withChallengeName("NEW_PASSWORD_REQUIRED")
+        .withChallengeResponses(challengeResponseParams)
+        .withSession(session)
+    val okResult = mIdentityProvider.adminRespondToAuthChallenge(request)
+  }
 
+  def findUserByEmailAddr(str: String): Option[SignUpForm.Data] = None
 
 }
